2011ea9ae8c8a254e7d7ce1b6130c021300094c0
robsongajunior <robson.ga.junior@gmail.com>
2015-07-06 23:18:18 -0300

    [+] add - init 1 responsive

--- resources/js/main.js
+++ resources/js/main.js
@@ -24,59 +24,6 @@ var HarmonicWebsite = function () {
 
 	/**
 	*
-	* PRIVATE METHODS
-	*
-	**/
-	PRIVATE.viewportDefine = function (windowSize) {
-		var bodyElement = D.getElementsByTagName('body')[0],
-			ww = windowSize.windowWidth;
-
-		console.log('### ww', windowSize);
-
-		switch(true) {
-			case ww >= 1190:
-				PUBLIC.setAttrElement(HTML, 'data-range', BREAKPOINTS.l);
-				break;
-			case ww >= 768 && ww < 1024:
-				PUBLIC.setAttrElement(HTML, 'data-range', BREAKPOINTS.m);
-				break;
-			case ww >= 320 && ww < 768:
-				PUBLIC.setAttrElement(HTML, 'data-range', BREAKPOINTS.s);
-				break;
-			default:
-				console.log('Sorry, your HTML size not exist.');
-		}
-	};
-
-
-	/**
-	*
-	* PUBLIC METHODS
-	*
-	**/
-	PUBLIC.windowSize = function () {
-		var ww = Math.max(
-		        D.body.scrollWidth, HTML.scrollWidth,
-				D.body.offsetWidth, HTML.offsetWidth,
-				D.body.clientWidth, HTML.clientWidth
-		    ),
-
-			wh = Math.max(
-		        BODY.scrollHeight, HTML.scrollHeight,
-		        BODY.offsetHeight, HTML.offsetHeight,
-		        BODY.clientHeight, HTML.clientHeight
-		    );
-
-		return {windowWidth: ww, windowHight: wh}
-	};
-
-	PUBLIC.setAttrElement = function (el, key, value) {
-		el.setAttribute(key, value);
-	};
-
-
-	/**
-	*
 	* CONSTRUCT
 	*
 	**/
